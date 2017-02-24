b5f99c003bbc485d2780c97da77c635f6347ec94
Pirozhok Igor <a074.pirozhok@outlook.com>
2017-02-24 17:24:17 +0200

    first commit

---
+++ app/src/main/java/com/bignerdranch/android/mlistener/SongListActivity.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.mlistener;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SongListActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_song_list);
+    }
+}
